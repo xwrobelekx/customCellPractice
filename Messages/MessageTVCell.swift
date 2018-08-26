9576ad755a02462e4792ac437dd7b439f2b87ce4
Kamil Wrobel <wrobelk@live.com>
2018-08-25 18:58:03 -0600

    first custom cell

---
+++ Messages/MessageTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageTVCell.swift
+//  Messages
+//
+//  Created by Kamil Wrobel on 8/25/18.
+//  Copyright © 2018 Kamil Wrobel. All rights reserved.
+//
+
+import UIKit
+
+class MessageTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
